3573de7117937d3b0a9d95744b20049685631624
doanah2000 <doanah1@uci.edu>
2024-10-07 18:18:33 -0700

    Adding name to website

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Andrew H. Doan
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
